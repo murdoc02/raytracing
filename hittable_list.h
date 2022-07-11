588ad9b8a48a77e611354e1eee2cf126f2e77082
richard <garzon.r@gmail.com>
2022-07-11 23:19:01 +0300

    use explicit namespaces

--- hittable_list.h
+++ hittable_list.h
@@ -6,22 +6,19 @@
 #include <memory>
 #include <vector>
 
-using std::shared_ptr;
-using std::make_shared;
-
 class hittable_list : public hittable {
     public:
         hittable_list() {}
-        hittable_list(shared_ptr<hittable> object) { add(object); }
+        hittable_list(std::shared_ptr<hittable> object) { add(object); }
 
         void clear() { objects.clear(); }
-        void add(shared_ptr<hittable> object) {objects.push_back(object); }
+        void add(std::shared_ptr<hittable> object) {objects.push_back(object); }
 
         virtual bool hit(
             const ray& r, double t_min, double t_max, hit_record& rec) const override;
 
     public:
-        std::vector<shared_ptr<hittable>> objects;
+        std::vector<std::shared_ptr<hittable>> objects;
 };
 
 bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
