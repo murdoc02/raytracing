457e2539a410e0b7fd24921e6e49561e3622f987
rg <garzon.r@gmail.com>
2022-07-26 19:38:50 -0400

    finish

--- camera.h
+++ camera.h
@@ -6,13 +6,9 @@
 class camera
 {
   public:
-    camera(point3 lookfrom,
-           point3 lookat,
-           vec3 vup,
+    camera(point3 lookfrom, point3 lookat, vec3 vup,
            double vfov, // vertical field-of-view in degrees
-           double aspect_ratio,
-           double aperture,
-           double focus_dist)
+           double aspect_ratio, double aperture, double focus_dist)
     {
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
@@ -21,13 +17,13 @@ class camera
 
         w = unit_vector(lookfrom - lookat);
         u = unit_vector(cross(vup, w));
-        v = cross(w,u);
+        v = cross(w, u);
 
         origin = lookfrom;
         horizontal = focus_dist * viewport_width * u;
         vertical = focus_dist * viewport_height * v;
         lower_left_corner =
-            origin - horizontal / 2 - vertical / 2 - focus_dist*w;
+            origin - horizontal / 2 - vertical / 2 - focus_dist * w;
         lens_radius = aperture / 2;
     }
 
@@ -36,8 +32,8 @@ class camera
         vec3 rd = lens_radius * random_in_unit_disk();
         vec3 offset = u * rd.x() + v * rd.y();
 
-        return ray(origin+offset,
-                   lower_left_corner + s * horizontal + t * vertical - origin - offset);
+        return ray(origin + offset, lower_left_corner + s * horizontal +
+                                        t * vertical - origin - offset);
     }
 
   private:
@@ -45,7 +41,7 @@ class camera
     point3 lower_left_corner;
     vec3 horizontal;
     vec3 vertical;
-    vec3 u,v,w;
+    vec3 u, v, w;
     double lens_radius;
 };
 #endif
